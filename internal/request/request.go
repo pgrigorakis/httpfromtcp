747cc0c583c6ff3f36913539340321d999d372f2
Panagiotis Grigorakis <p.grigorakis@gmail.com>
2026-05-22 00:35:04 +0200

    Parse request line incrementally

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,14 +1,23 @@
 package request
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"strings"
 	"unicode"
 )
 
+type InitState int
+
+const (
+	StateInitialised InitState = iota
+	StateDone
+)
+
 type Request struct {
 	RequestLine RequestLine
+	InitState   int
 }
 
 type RequestLine struct {
@@ -17,35 +26,79 @@ type RequestLine struct {
 	Method        string
 }
 
-func RequestFromReader(reader io.Reader) (*Request, error) {
-	b, err := io.ReadAll(reader)
-	if err != nil {
-		fmt.Printf("error: could not read request - %s", err.Error())
-		return &Request{}, err
-	}
+const bufferSize = 8
 
-	requestLine, err := parseRequestLine(string(b))
-	if err != nil {
-		fmt.Printf("error: could not parse request - %s", err.Error())
-		return &Request{}, err
-	}
+func RequestFromReader(reader io.Reader) (*Request, error) {
+	buf := make([]byte, bufferSize)
+	readToIndex := 0
 
 	r := Request{
-		RequestLine: *requestLine}
+		RequestLine: RequestLine{},
+		InitState:   int(StateInitialised),
+	}
 
+	for r.InitState != int(StateDone) {
+		if readToIndex == len(buf) {
+			newBuf := make([]byte, len(buf)*2)
+			copy(newBuf, buf[:readToIndex])
+			buf = newBuf
+		}
+
+		n, err := reader.Read(buf[readToIndex:])
+		if err == io.EOF {
+			r.InitState = int(StateDone)
+			break
+		}
+
+		readToIndex += n
+
+		parsedBytes, err := r.parse(buf[:readToIndex])
+		if err != nil {
+			return &r, err
+		}
+		copy(buf, buf[parsedBytes:readToIndex])
+		readToIndex -= parsedBytes
+	}
 	return &r, nil
 }
 
-func parseRequestLine(requestText string) (*RequestLine, error) {
-	requestLine := strings.Split(requestText, "\r\n")
-	if len(requestLine) == 0 || requestLine[0] == "" {
-		return nil, fmt.Errorf("empty request")
+func (r *Request) parse(data []byte) (int, error) {
+	if r.InitState == int(StateInitialised) {
+		requestLine, parsedBytes, err := parseRequestLine(data)
+		if err != nil {
+			return 0, err
+		}
+		if parsedBytes == 0 {
+			return 0, nil
+		} else {
+			r.RequestLine = *requestLine
+			r.InitState = int(StateDone)
+			return parsedBytes, nil
+		}
+	} else if r.InitState == int(StateDone) {
+		return 0, fmt.Errorf("error: trying to read data in a done state")
+	} else {
+		return 0, fmt.Errorf("error: unknown state")
+	}
+}
+
+func parseRequestLine(requestBytes []byte) (*RequestLine, int, error) {
+	idx := bytes.Index(requestBytes, []byte("\r\n"))
+	if idx == -1 {
+		return nil, 0, nil
 	}
 
-	requestLineParts := strings.Fields(requestLine[0])
-	if len(requestLineParts) == 0 {
-		return nil, fmt.Errorf("empty request line")
+	requestText := string(requestBytes[:idx])
+	requestLine, err := requestLineFromString(requestText)
+	if err != nil {
+		return nil, 0, err
 	}
+
+	return requestLine, idx + 2, nil
+}
+
+func requestLineFromString(str string) (*RequestLine, error) {
+	requestLineParts := strings.Split(str, " ")
 	if len(requestLineParts) != 3 {
 		return nil, fmt.Errorf("incomplete request")
 	}
