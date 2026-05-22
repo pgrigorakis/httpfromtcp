362ce36b90a2b41b668c48354606337c05b94e06
Panagiotis Grigorakis <p.grigorakis@gmail.com>
2026-05-22 16:10:22 +0200

    Use request parser in TCP listener

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"io"
 	"log"
 	"net"
-	"strings"
+
+	"github.com/pgrigorakis/httpfromtcp/internal/request"
 )
 
 const port = ":42069"
@@ -28,44 +27,18 @@ func main() {
 		}
 		fmt.Println("Connection has been accepted from", conn.RemoteAddr())
 
-		linesChan := getLinesChannel(conn)
-		for line := range linesChan {
-			fmt.Println(line)
+		parsedRequest, err := request.RequestFromReader(conn)
+		if err != nil {
+			log.Fatalf("couldn't parse request. error: %s", err.Error())
+			return
 		}
+
+		fmt.Println("Request line:")
+		fmt.Printf("- Method: %s\n", parsedRequest.RequestLine.Method)
+		fmt.Printf("- Target: %s\n", parsedRequest.RequestLine.RequestTarget)
+		fmt.Printf("- Version: %s\n", parsedRequest.RequestLine.HttpVersion)
 
 		fmt.Println("Connection to ", conn.RemoteAddr(), "closed.")
 
 	}
-}
-
-func getLinesChannel(conn io.ReadCloser) <-chan string {
-	lines := make(chan string)
-
-	go func() {
-		defer close(lines)
-		defer conn.Close()
-		currentLine := ""
-		for {
-			data := make([]byte, 8)
-			n, err := conn.Read(data)
-			if err != nil {
-				if currentLine != "" {
-					lines <- currentLine
-				}
-				if errors.Is(err, io.EOF) {
-					break
-				}
-				fmt.Printf("error: %s\n", err.Error())
-				return
-			}
-			parts := strings.Split(string(data[:n]), "\n")
-			for i := 0; i < len(parts)-1; i++ {
-				currentLine += parts[i]
-				lines <- currentLine
-				currentLine = ""
-			}
-			currentLine += parts[len(parts)-1]
-		}
-	}()
-	return lines
 }
